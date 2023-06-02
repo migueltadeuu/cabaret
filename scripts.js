fe80a1f83445877dc219fcd070797a8060ea8135
migueltadeuu <miguel.tadeu5002@gmail.com>
2023-06-02 16:54:04 +0100

    I added a filter to the code, some photos and also a form that allows you to book a test drive

---
+++ scripts.js
@@ -0,0 +1,640 @@
+    const coleção = []
+
+    //ASTON MARTIN SUPERLEGGERA SPECS
+    const superleggera = {
+        marca: "Aston Martin",
+        modelo: "Superleggera",
+        motor: "Combustão",
+        tipo_do_motor: "5.2L V12 Twin-Turbo",
+        cavalos: "715 HP",
+        tração: "RWD",
+        top_speed: "340 Km/h",
+        zero_aos_cem: "3,2 segundos",
+        peso: "1700 kg",
+        SUV_true: false,
+        path_name: "superleggera",
+    }
+    coleção.push(superleggera);
+
+
+    //ASTON MARTIN DBX SPECS
+    const dbx = {
+        marca: "Aston Martin",
+        modelo: "DBX",
+        motor: "Combustão",
+        tipo_do_motor: "4.0L V8 Twin-Turbo",
+        cavalos: "540 HP",
+        tração: "RWD",
+        top_speed: "291 Km/h",
+        zero_aos_cem: "4,3 segundos",
+        peso: "2240 kg",
+        SUV_true: true,
+        path_name: "dbx",
+    }
+    coleção.push(dbx);
+
+
+    //ASTON MARTIN VANTAGE SPECS
+    const vantage = {
+        marca: "Aston Martin",
+        modelo: "Vantage F1 Edition",
+        motor: "Combustão",
+        tipo_do_motor: "4.0L V8 Twin-Turbo",
+        cavalos: "550 HP",
+        tração: "RWD",
+        top_speed: "315 Km/h",
+        zero_aos_cem: "3,5 segundos",
+        peso: "1630 kg",
+        SUV_true: false,
+        path_name: "vantage",
+    }
+    coleção.push(vantage);   
+
+    
+    //BENTLEY BENTAYGA SPECS
+    const bentayga = {
+        marca: "Bentley",
+        modelo: "Bentayga",
+        motor: "Combustão",
+        tipo_do_motor: "4.0L V8 Twin-Turbo",
+        cavalos: "625 HP",
+        tração: "RWD",
+        top_speed: "306 Km/h",
+        zero_aos_cem: "4,4 segundos",
+        peso: "2450 kg",
+        SUV_true: true,
+        path_name: "bentayga",
+    }
+    coleção.push(bentayga);
+
+
+    //BENTLEY CONTINENTAL GT SPECS
+    const continental = {
+        marca: "Bentley",
+        modelo: "Continental GT",
+        motor: "Combustão",
+        tipo_do_motor: "6.0L W12 Twin-Turbo",
+        cavalos: "625 HP",
+        tração: "RWD",
+        top_speed: "333 Km/h",
+        zero_aos_cem: "3,3 segundos",
+        peso: "2270 kg",
+        SUV_true: false,
+        path_name: "continental",
+    }
+    coleção.push(continental);
+
+
+    //BENTLEY CONTINENTAL GT SPECS
+    const flying_spur = {
+        marca: "Bentley",
+        modelo: "Flying Spur",
+        motor: "Combustão",
+        tipo_do_motor: "4.0L V8 Twin-Turbo",
+        cavalos: "540 HP",
+        tração: "AWD",
+        top_speed: "318 Km/h",
+        zero_aos_cem: "3,7 segundos",
+        peso: "2480 kg",
+        SUV_true: false,
+        path_name: "flying_spur",
+    }
+    coleção.push(flying_spur);
+
+
+    //BUGATTI CHIRON SUPER SPORT 300+ SPECS
+    const chiron = {
+        marca: "Bugatti",
+        modelo: "Chiron Super Sport 300+",
+        motor: "Combustão",
+        tipo_do_motor: "8.0L W16 Quad-Turbo",
+        cavalos: "1500 HP",
+        tração: "AWD",
+        top_speed: "482 Km/h",
+        zero_aos_cem: "2,5 segundos",
+        peso: "2000 kg",
+        SUV_true: false,
+        path_name: "chiron_300",
+    }
+    coleção.push(chiron);
+
+
+    //BUGATTI VEYRON SUPER SPORT SPECS
+    const veyron = {
+        marca: "Bugatti",
+        modelo: "Veyron Super Sport",
+        motor: "Combustão",
+        tipo_do_motor: "8.0L W16 Quad-Turbo",
+        cavalos: "1200 HP",
+        tração: "AWD",
+        top_speed: "415 Km/h",
+        zero_aos_cem: "2,6 segundos",
+        peso: "1900 kg",
+        SUV_true: false,
+        path_name: "veyron",
+    }
+    coleção.push(veyron);
+
+
+    //BUGATTI CENTODIECI SPECS
+    const centodieci = {
+        marca: "Bugatti",
+        modelo: "Centodieci",
+        motor: "Combustão",
+        tipo_do_motor: "8.0L W16 Quad-Turbo",
+        cavalos: "1600 HP",
+        tração: "AWD",
+        top_speed: "380 Km/h",
+        zero_aos_cem: "2,4 segundos",
+        peso: "1700 kg",
+        SUV_true: false,
+        path_name: "centodieci",
+    }
+    coleção.push(centodieci);
+
+
+    //FERRARI 488 PISTA SPECS
+    const _488pista = {
+        marca: "Ferrari",
+        modelo: "488 Pista",
+        motor: "Combustão",
+        tipo_do_motor: "3.9L V8 Twin-Turbo",
+        cavalos: "710 HP",
+        tração: "RWD",
+        top_speed: "340 Km/h",
+        zero_aos_cem: "2,9 segundos",
+        peso: "1300 kg",
+        SUV_true: false,
+        path_name: "488_pista",
+    }
+    coleção.push(_488pista);
+
+
+    //FERRARI 812 COMPETIZIONE SPECS
+    const _812competizione = {
+        marca: "Ferrari",
+        modelo: "812 Competizione",
+        motor: "Combustão",
+        tipo_do_motor: "6.5L V12 Naturally Aspirated",
+        cavalos: "820 HP",
+        tração: "RWD",
+        top_speed: "340 Km/h",
+        zero_aos_cem: "2,9 segundos",
+        peso: "1530 kg",
+        SUV_true: false,
+        path_name: "812_competizione",
+    }
+    coleção.push(_812competizione);
+
+
+    //FERRARI FXX-K EVO SPECS
+    const fxx_k_evo = {
+        marca: "Ferrari",
+        modelo: "FXX-K Evo",
+        motor: "Combustão",
+        tipo_do_motor: "6.3L V12 Naturally Aspirated",
+        cavalos: "820 HP",
+        tração: "RWD",
+        top_speed: "340 Km/h",
+        zero_aos_cem: "2,9 segundos",
+        peso: "1530 kg",
+        SUV_true: false,
+        path_name: "fxx-k_evo",
+    }
+    coleção.push(fxx_k_evo);
+
+
+    //LAMBORGHINI AVENTADOR SVJ SPECS
+    const svj = {
+        marca: "Lamborghini",
+        modelo: "Aventador SVJ",
+        motor: "Combustão",
+        tipo_do_motor: "6.5L V12 Naturally Aspirated",
+        cavalos: "770 HP",
+        tração: "AWD",
+        top_speed: "350 Km/h",
+        zero_aos_cem: "2,8 segundos",
+        peso: "1750 kg",
+        SUV_true: false,
+        path_name: "aventador_svj",
+    }
+    coleção.push(svj);
+
+
+    //LAMBORGHINI URUS SPECS
+    const urus = {
+        marca: "Lamborghini",
+        modelo: "Urus",
+        motor: "Combustão",
+        tipo_do_motor: "4.0L V8 Twin-Turbo",
+        cavalos: "640 HP",
+        tração: "AWD",
+        top_speed: "305 Km/h",
+        zero_aos_cem: "3,6 segundos",
+        peso: "2200 kg",
+        SUV_true: true,
+        path_name: "urus",
+    }
+    coleção.push(urus);
+
+
+    //LAMBORGHINI MURCIELAGO SV SPECS
+    const murcielago = {
+        marca: "Lamborghini",
+        modelo: "Murciélago",
+        motor: "Combustão",
+        tipo_do_motor: "6.5L V12 Naturally Aspirated",
+        cavalos: "660 HP",
+        tração: "AWD",
+        top_speed: "345 Km/h",
+        zero_aos_cem: "3,2 segundos",
+        peso: "1600 kg",
+        SUV_true: false,
+        path_name: "murcielago",
+    }
+    coleção.push(murcielago);
+
+
+    //MASERATI GRANTURISMO SPECS
+    const granturismo = {
+        marca: "Maserati",
+        modelo: "GranTurismo",
+        motor: "Combustão",
+        tipo_do_motor: "4.7L V8 Naturally Aspirated",
+        cavalos: "450 HP",
+        tração: "RWD",
+        top_speed: "300 Km/h",
+        zero_aos_cem: "4,7 segundos",
+        peso: "1880 kg",
+        SUV_true: false,
+        path_name: "granturismo",
+    }
+    coleção.push(granturismo);
+
+
+    //MASERATI LEVANTE SPECS
+    const levante = {
+        marca: "Maserati",
+        modelo: "Levante",
+        motor: "Combustão",
+        tipo_do_motor: "3.8L V8 Twin-Turbo",
+        cavalos: "580 HP",
+        tração: "AWD",
+        top_speed: "300 Km/h",
+        zero_aos_cem: "3,7 segundos",
+        peso: "2170 kg",
+        SUV_true: true,
+        path_name: "levante",
+    }
+    coleção.push(levante);
+
+
+    //MASERATI MC20 SPECS
+    const mc20 = {
+        marca: "Maserati",
+        modelo: "MC20",
+        motor: "Combustão",
+        tipo_do_motor: "3.0L V6 Twin-Turbo",
+        cavalos: "620 HP",
+        tração: "RWD",
+        top_speed: "325 Km/h",
+        zero_aos_cem: "2,9 segundos",
+        peso: "1500 kg",
+        SUV_true: false,
+        path_name: "mc20",
+    }
+    coleção.push(mc20);
+
+
+     //MCLAREN 765LT SPECS
+     const _765lt = {
+        marca: "McLaren",
+        modelo: "765LT",
+        motor: "Combustão",
+        tipo_do_motor: "4.0L V8 Twin-Turbo",
+        cavalos: "755 HP",
+        tração: "RWD",
+        top_speed: "330 Km/h",
+        zero_aos_cem: "2,7 segundos",
+        peso: "1340 kg",
+        SUV_true: false,
+        path_name: "765lt",
+    }
+    coleção.push(_765lt);
+
+
+    //MCLAREN P1 SPECS
+    const p1 = {
+        marca: "McLaren",
+        modelo: "P1",
+        motor: "Combustão",
+        tipo_do_motor: "3.8L V8 Twin-Turbo",
+        cavalos: "900 HP",
+        tração: "RWD",
+        top_speed: "350 Km/h",
+        zero_aos_cem: "2,7 segundos",
+        peso: "1550 kg",
+        SUV_true: false,
+        path_name: "p1",
+    }
+    coleção.push(p1);
+
+
+    //MCLAREN SENNA GTR SPECS
+    const senna = {
+        marca: "McLaren",
+        modelo: "Senna GTR",
+        motor: "Combustão",
+        tipo_do_motor: "4.0L V8 Twin-Turbo",
+        cavalos: "815 HP",
+        tração: "RWD",
+        top_speed: "340 Km/h",
+        zero_aos_cem: "2,8 segundos",
+        peso: "1180 kg",
+        SUV_true: false,
+        path_name: "senna_gtr",
+    }
+    coleção.push(senna);
+
+
+    //MERCEDES-AMG ONE SPECS
+    const amg_one = {
+        marca: "Mercedes",
+        modelo: "AMG ONE",
+        motor: "Combustão",
+        tipo_do_motor: "1.6L V6 Turbo + 4 eletric motors",
+        cavalos: "1050 HP",
+        tração: "AWD",
+        top_speed: "350 Km/h",
+        zero_aos_cem: "2,6 segundos",
+        peso: "1700 kg",
+        SUV_true: false,
+        path_name: "amg_one",
+    }
+    coleção.push(amg_one);
+
+
+    //MERCEDES G63 AMG SPECS
+    const g_wagon = {
+        marca: "Mercedes",
+        modelo: "G63 AMG",
+        motor: "Combustão",
+        tipo_do_motor: "4.0L V8 Twin-Turbo",
+        cavalos: "575 HP",
+        tração: "AWD",
+        top_speed: "240 Km/h",
+        zero_aos_cem: "4,4 segundos",
+        peso: "2650 kg",
+        SUV_true: true,
+        path_name: "g_wagon",
+    }
+    coleção.push(g_wagon);
+
+
+    //MERCEDES-AMG BLACK SERIES SPECS
+    const bs = {
+        marca: "Mercedes",
+        modelo: "AMG GT Black Series",
+        motor: "Combustão",
+        tipo_do_motor: "4.0L V8 Twin-Turbo",
+        cavalos: "720 HP",
+        tração: "RWD",
+        top_speed: "325 Km/h",
+        zero_aos_cem: "3,1 segundos",
+        peso: "1775 kg",
+        SUV_true: false,
+        path_name: "BS",
+    }
+    coleção.push(bs);
+
+
+    //PAGANI ZONDA SPECS
+    const zonda = {
+        marca: "Pagani",
+        modelo: "Zonda",
+        motor: "Combustão",
+        tipo_do_motor: "6.0L V12 Naturally Aspirated",
+        cavalos: "800 HP",
+        tração: "RWD",
+        top_speed: "320 Km/h",
+        zero_aos_cem: "2,8 segundos",
+        peso: "1400 kg",
+        SUV_true: false,
+        path_name: "zonda",
+    }
+    coleção.push(zonda);
+
+
+    //PAGANI HUAYRA R SPECS
+    const huayra = {
+        marca: "Pagani",
+        modelo: "Huayra R",
+        motor: "Combustão",
+        tipo_do_motor: "6.0L V12 Naturally Aspirated",
+        cavalos: "850 HP",
+        tração: "RWD",
+        top_speed: "385 Km/h",
+        zero_aos_cem: "2,8 segundos",
+        peso: "1050 kg",
+        SUV_true: false,
+        path_name: "huayra",
+    }
+    coleção.push(huayra);
+
+
+    //PAGANI UTOPIA SPECS
+    const utopia = {
+        marca: "Pagani",
+        modelo: "Utopia",
+        motor: "Combustão",
+        tipo_do_motor: "6.0L V12 Twin-Turbo",
+        cavalos: "870 HP",
+        tração: "RWD",
+        top_speed: "355 Km/h",
+        zero_aos_cem: "3,0 segundos",
+        peso: "1280 kg",
+        SUV_true: false,
+        path_name: "utopia",
+    }
+    coleção.push(utopia);
+
+
+    //PORSCHE MACAN GTS SPECS
+    const macan = {
+        marca: "Porsche",
+        modelo: "Macan GTS",
+        motor: "Combustão",
+        tipo_do_motor: "2.9L V6 Twin-Turbo",
+        cavalos: "430 HP",
+        tração: "AWD",
+        top_speed: "270 Km/h",
+        zero_aos_cem: "4,3 segundos",
+        peso: "1950 kg",
+        SUV_true: true,
+        path_name: "macan",
+    }
+    coleção.push(macan);
+
+
+    //PORSCHE 911 (992) GT3 RS SPECS
+    const gt3rs = {
+        marca: "Porsche",
+        modelo: "911 (992) GT3 RS",
+        motor: "Combustão",
+        tipo_do_motor: "4.0L F6 Naturally Aspirated",
+        cavalos: "530 HP",
+        tração: "RWD",
+        top_speed: "305 Km/h",
+        zero_aos_cem: "3,2 segundos",
+        peso: "1450 kg",
+        SUV_true: false,
+        path_name: "gt3rs",
+    }
+    coleção.push(gt3rs);
+
+
+    //PORSCHE TAYCAN TURBO S SPECS
+    const taycan = {
+        marca: "Porsche",
+        modelo: "Taycan Turbo S",
+        motor: "Elétrico",
+        tipo_do_motor: "Dual eletric motors",
+        cavalos: "530 HP",
+        tração: "AWD",
+        top_speed: "260 Km/h",
+        zero_aos_cem: "2,6 segundos",
+        peso: "2320 kg",
+        SUV_true: false,
+        path_name: "taycan",
+    }
+    coleção.push(taycan);
+
+
+    //ROLLS ROYCE WRAITH SPECS
+    const wraith = {
+        marca: "Rolls Royce",
+        modelo: "Wraith",
+        motor: "Combustão",
+        tipo_do_motor: "6.6L V12 Twin-Turbo",
+        cavalos: "625 HP",
+        tração: "RWD",
+        top_speed: "250 Km/h",
+        zero_aos_cem: "4,4 segundos",
+        peso: "2440 kg",
+        SUV_true: false,
+        path_name: "wraith",
+    }
+    coleção.push(wraith);
+
+
+    //ROLLS ROYCE PHANTOM SPECS
+    const phantom = {
+        marca: "Rolls Royce",
+        modelo: "Phantom",
+        motor: "Combustão",
+        tipo_do_motor: "6.6L V12 Twin-Turbo",
+        cavalos: "560 HP",
+        tração: "RWD",
+        top_speed: "250 Km/h",
+        zero_aos_cem: "5,1 segundos",
+        peso: "2745 kg",
+        SUV_true: false,
+        path_name: "phantom",
+    }
+    coleção.push(phantom); 
+
+
+    //ROLLS ROYCE CULLINAN SPECS
+    const cullinan = {
+        marca: "Rolls Royce",
+        modelo: "Cullinan",
+        motor: "Combustão",
+        tipo_do_motor: "6.6L V12 Twin-Turbo",
+        cavalos: "560 HP",
+        tração: "RWD",
+        top_speed: "250 Km/h",
+        zero_aos_cem: "5,1 segundos",
+        peso: "2745 kg",
+        SUV_true: true,
+        path_name: "cullinan",
+    }
+    coleção.push(cullinan); 
+
+
+
+
+
+    
+    function verificar_marca() {
+        var lista_de_modelos = document.getElementById("input1");
+        var input2 = document.getElementById("input2");
+        var datalist_modelos = document.getElementById("datalist_modelos");
+
+    
+        if (lista_de_modelos.value == '') {
+            input2.disabled = true;
+            input2.value = "";
+            datalist_modelos.innerHTML = ''; // Clear existing options
+            return;
+        }
+        
+        input2.disabled = false;
+        datalist_modelos.innerHTML = '';
+        coleção.forEach((carro) => {
+            if (carro.marca == lista_de_modelos.value)
+            {
+                const option = document.createElement("option");
+                option.text = carro.modelo;
+                datalist_modelos.appendChild(option);
+            }
+        })          
+    }
+
+    function aplicar_filtros() 
+    {
+        var lista_de_modelos = document.getElementById("input1");
+        var input2 = document.getElementById("input2");
+        var datalist_modelos = document.getElementById("datalist_modelos");
+        // var motor_ = document.getElementsByName("rad");
+        var SUV_ = document.getElementById("is_SUV");
+        var table = document.getElementById("show_filtro");
+
+        table.innerHTML = "";
+        coleção.forEach((carro) => {
+            var motor_combustao = document.getElementById("combustao");
+            var motor_eletrico = document.getElementById("eletrico");
+            var motor_list = [motor_combustao, motor_eletrico];
+                motor_checked = motor_list.find((item) => item.checked == true );
+            if (carro.marca == lista_de_modelos.value && carro.modelo == "")// && carro.SUV == "" && carro.tipo_do_motor == "")
+            {
+                var table = document.getElementById("show_filtro");
+                var newRow = document.createElement("tr");
+                var image = document.createElement("img");
+                image.src = "Imagens/carros/" + carro.marca + "/" + carro.path_name + ".jpg";
+                newRow.appendChild(image);
+                table.appendChild(newRow);
+            } 
+            else if (carro.marca == lista_de_modelos.value && carro.modelo == input2.value && carro.motor == "") 
+            {
+                var table = document.getElementById("show_filtro");
+                var newRow = document.createElement("tr");
+                var image = document.createElement("img");
+                image.src = "Imagens/carros/" + lista_de_modelos.value + "/" + carro.path_name + ".jpg";
+                newRow.appendChild(image);
+                table.appendChild(newRow);
+            }
+            else if (carro.marca == lista_de_modelos.value && carro.modelo == input2.value && carro.motor == motor_checked.value) 
+            {
+                var table = document.getElementById("show_filtro");
+                var newRow = document.createElement("tr");
+                var image = document.createElement("img");
+                image.src = "Imagens/carros/" + lista_de_modelos.value + "/" + carro.path_name + ".jpg";
+                newRow.appendChild(image);
+                table.appendChild(newRow);
+            } else {
+                console.log("erro!");
+            }
+
+        })
+    }
+    
